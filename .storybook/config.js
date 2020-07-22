4be30ce59eac8c0bc31d9bd7bd3fa8a4c28ba538
David Salazar <davidjsalazarmoreno@gmail.com>
2020-07-21 20:54:28 -0500

    Remove old stuff

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@kadira/storybook';
-
-import 'normalize.css/normalize.css';
-
-function loadStories() {
-  require('../src/stories/menu.story');
-}
-
-configure(loadStories, module);
--- translations.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-node_modules/.bin/tsc -p tsconfig.intl.json
-node_modules/.bin/babel "trans-tmp/**/*.jsx"
-node_modules/.bin/babel-node translations/translate.js;
-rm -rf trans-tmp
